b26c87033516f83f07bdf54761bb7d2073eb715c
Shannon Smith <infiltratr@yandex.com>
2026-09-01 14:41:54 +1000

    Expose STM32 ReadDTCInformation test API

--- examples/stm32c092/link-stm32c092-example.h
+++ examples/stm32c092/link-stm32c092-example.h
@@ -3,6 +3,7 @@
 #define LINK_STM32C092_EXAMPLE_H
 
 #include "fdcan.h"
+#include "link/uds_dtc.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -15,11 +16,15 @@ typedef enum {
     LINK_STM32C092_EXAMPLE_IDLE = 0,
     LINK_STM32C092_EXAMPLE_READING_VIN,
     LINK_STM32C092_EXAMPLE_VIN_READY,
+    LINK_STM32C092_EXAMPLE_READING_DTC,
+    LINK_STM32C092_EXAMPLE_DTC_READY,
     LINK_STM32C092_EXAMPLE_NEGATIVE_RESPONSE,
     LINK_STM32C092_EXAMPLE_FAILED
 } LinkStm32C092ExampleState;
 
 bool link_stm32c092_example_init(FDCAN_HandleTypeDef *hfdcan);
+bool link_stm32c092_example_start_dtc_report(
+    const LinkUdsDtcInformationRequest *request);
 void link_stm32c092_example_process(void);
 void link_stm32c092_example_rx_fifo0_irq(FDCAN_HandleTypeDef *hfdcan);
 void link_stm32c092_example_tx_event_irq(
@@ -27,6 +32,9 @@ void link_stm32c092_example_tx_event_irq(
     uint32_t tx_event_fifo_its);
 LinkStm32C092ExampleState link_stm32c092_example_state(void);
 const char *link_stm32c092_example_vin(void);
+const LinkUdsDtcInformationResponse *
+link_stm32c092_example_dtc_response(void);
+LinkUdsResult link_stm32c092_example_dtc_decode_result(void);
 uint8_t link_stm32c092_example_negative_response_code(void);
 uint32_t link_stm32c092_example_dropped_frames(void);
 
