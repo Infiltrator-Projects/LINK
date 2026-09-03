b2ff47c0c10e394079d85029d7724a7b1269bfe3
Shannon Smith <infiltratr@yandex.com>
2026-09-04 08:59:30 +1000

    apple: own portable UDS amalgamation

---
+++ platform/apple/LinkPortableUds.c
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/* LINK-owned Apple UDS amalgamation for product faces. */
+#include "../../src/uds/uds.c"
+#include "../../src/uds/uds_services.c"
+#include "../../src/uds/uds_server.c"
