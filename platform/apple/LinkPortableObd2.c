ee117efecd3e4621e2e65eeb581e0e31482ee42c
Shannon Smith <infiltratr@yandex.com>
2026-09-04 08:59:23 +1000

    apple: own portable OBD-II amalgamation

---
+++ platform/apple/LinkPortableObd2.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/* LINK-owned Apple OBD-II amalgamation for product faces. */
+#include "../../src/obd2/obd2.c"
+#include "../../src/obd2/pid_catalogue.c"
+#include "../../src/obd2/j1979da.c"
+#include "../../src/obd2/dtc_knowledge.c"
