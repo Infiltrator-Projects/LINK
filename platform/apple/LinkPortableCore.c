4d1b88b515c1e59df2362a03891e6ca45c643af5
Shannon Smith <infiltratr@yandex.com>
2026-09-06 20:14:47 +1000

    Support split consumers of Apple portable core

--- platform/apple/LinkPortableCore.c
+++ platform/apple/LinkPortableCore.c
@@ -2,17 +2,34 @@
 /* LINK-owned Apple portable-core amalgamation for product faces. */
 #include "link/features.h"
 
+/*
+ * A product may already compile LINK's i18n or ISO-TP implementation as a
+ * dedicated Xcode source.  Explicit opt-outs let that product consume this
+ * LINK-owned amalgamation without creating duplicate symbols while preserving
+ * the default single-translation-unit topology for new product faces.
+ */
+#ifndef LINK_APPLE_PORTABLE_CORE_EXTERNAL_I18N
+#define LINK_APPLE_PORTABLE_CORE_EXTERNAL_I18N 0
+#endif
+#ifndef LINK_APPLE_PORTABLE_CORE_EXTERNAL_ISOTP
+#define LINK_APPLE_PORTABLE_CORE_EXTERNAL_ISOTP 0
+#endif
+
 #include "../../src/core/workspace.c"
+#if !LINK_APPLE_PORTABLE_CORE_EXTERNAL_I18N
 #include "../../src/core/i18n.c"
 #include "../../src/core/i18n_platform.c"
 #include "../../src/core/language_pack.c"
+#endif
 #include "../../src/core/units.c"
 #include "../../src/core/fuel_economy.c"
 #include "../../src/core/diagnostic_request.c"
 #include "../../src/core/doip.c"
 #include "../../src/core/diagnostic_flow.c"
 #include "../../src/core/diagnostic_capability.c"
+#if !LINK_APPLE_PORTABLE_CORE_EXTERNAL_ISOTP
 #include "../../src/core/isotp.c"
+#endif
 #include "../../src/core/parameter.c"
 #include "../../src/core/dashboard.c"
 #include "../../src/core/scheduler.c"
