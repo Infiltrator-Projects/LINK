7053a8b35cd7d788f64030906971dc302d06389b
Shannon Smith <infiltratr@yandex.com>
2026-09-22 14:10:06 +1000

    Add issue-27 Cube compile stubs

---
+++ tests/stm32c092_issue27_stub/main.h
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef LINK_TEST_STM32C092_ISSUE27_MAIN_H
+#define LINK_TEST_STM32C092_ISSUE27_MAIN_H
+#include <stdint.h>
+typedef enum { HAL_OK = 0, HAL_ERROR = 1 } HAL_StatusTypeDef;
+typedef struct { uint32_t OscillatorType; uint32_t HSEState; } RCC_OscInitTypeDef;
+typedef struct {
+    uint32_t ClockType;
+    uint32_t SYSCLKSource;
+    uint32_t SYSCLKDivider;
+    uint32_t AHBCLKDivider;
+    uint32_t APB1CLKDivider;
+} RCC_ClkInitTypeDef;
+#define FLASH_LATENCY_1 UINT32_C(1)
+#define RCC_OSCILLATORTYPE_HSE UINT32_C(1)
+#define RCC_HSE_ON UINT32_C(1)
+#define RCC_CLOCKTYPE_HCLK UINT32_C(1)
+#define RCC_CLOCKTYPE_SYSCLK UINT32_C(2)
+#define RCC_CLOCKTYPE_PCLK1 UINT32_C(4)
+#define RCC_SYSCLKSOURCE_HSE UINT32_C(1)
+#define RCC_SYSCLK_DIV1 UINT32_C(1)
+#define RCC_HCLK_DIV1 UINT32_C(1)
+#define __HAL_FLASH_SET_LATENCY(x) ((void)(x))
+void HAL_Init(void);
+HAL_StatusTypeDef HAL_RCC_OscConfig(RCC_OscInitTypeDef *config);
+HAL_StatusTypeDef HAL_RCC_ClockConfig(RCC_ClkInitTypeDef *config, uint32_t latency);
+void __disable_irq(void);
+void Error_Handler(void);
+#endif
